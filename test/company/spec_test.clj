107311a39f8dfc9e334d0fac49e40afae8492be3
Diego Sanchez <>
2020-05-05 17:41:58 -0300

    spec deps added

--- test/company/spec_test.clj
+++ test/company/spec_test.clj
@@ -1,7 +1,14 @@
 (ns company.spec-test
-  (:require [clojure.test :refer :all]))
+  (:require [clojure.test :refer :all]
+            [clojure.spec.alpha :as s]
+            [clojure.spec.test.alpha :as stest]))
+
 
 
 (deftest adding-numbers
   (is (= 4 (+ 2 2))))
+
+(deftest conform-test-1
+  (is (s/conform even? 100)))
+
 
