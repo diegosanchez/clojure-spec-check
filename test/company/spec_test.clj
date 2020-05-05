b767bf29e85101724a4b217a6094227db0d2c3ca
Diego Sanchez <>
2020-05-05 18:12:51 -0300

    def ::index-of-args

--- test/company/spec_test.clj
+++ test/company/spec_test.clj
@@ -1,9 +1,21 @@
 (ns company.spec-test
-  (:require [clojure.test :refer :all]
+  (:require [clojure.repl :refer :all]
+            [clojure.string :as str]
+            [clojure.test :refer :all]
             [clojure.spec.alpha :as s]
             [clojure.spec.test.alpha :as stest]))
 
 
+(defn my-index-of
+  [source search]
+  (str/index-of source search))
+
+(s/def ::index-of-args (s/cat :source string? :search string?))
+
+(s/fdef my-index-of
+  :args (s/cat :source string? :search string?)
+  :ret nat-int?
+  :fn #(<= (:ret %) (-> % :args :source count)))
 
 (deftest adding-numbers
   (is (= 4 (+ 2 2))))
@@ -11,4 +23,22 @@
 (deftest conform-test-1
   (is (s/conform even? 100)))
 
+(deftest my-index-of-test-1
+  (is (= (my-index-of "foobar" "b") 3)))
+
+(deftest my-index-of-test-2
+  (is (= (apply my-index-of ["foobar" "b"]) 3)))
+
+(deftest my-index-of-test-3
+  (is (= (apply my-index-of ["foobar" "f"]) 0)))
+
+(deftest index-of-args-test-1
+  (is (s/valid? ::index-of-args ["foo" "f"])))
+
+(deftest index-of-args-test-2
+  (is (not (s/valid? ::index-of-args ["foo" 3]))))
+
+(deftest index-of-args-test-conform-1
+  (let [expected {:source "foo", :search "f"}]
+    (is (= (s/conform ::index-of-args ["foo" "f"]) expected))))
 
