cfc8d77300f63450461fd8a6a00ed9ce5c330894
Diego Sanchez <diego.sanchez@etermax.com>
2020-07-03 16:19:18 -0300

    Adding return values

--- test/company/spec_magicum_test.clj
+++ test/company/spec_magicum_test.clj
@@ -5,18 +5,22 @@
             [clojure.spec.alpha :as s]
             [clojure.spec.test.alpha :as stest]))
 
-(require '[clojure.string :as str])
-(require '[clojure.spec.alpha :as s])
-(require '[clojure.spec.test.alpha :as stest])
-(require '[clojure.spec.gen.alpha :as gen])
+;; (require '[clojure.string :as str])
+;; (require '[clojure.spec.alpha :as s])
+;; (require '[clojure.spec.test.alpha :as stest])
+;; (require '[clojure.spec.gen.alpha :as gen])
+
+(s/def ::board (s/coll-of int?))
+(s/def ::hand (s/coll-of int?))
+(s/def ::game (s/keys :req [::board ::hand]))
 
 
 (defn laid-down [board hand]
-  1)
+  {::board board, ::hand hand})
 
 (s/fdef laid-down
-  :args (s/cat :board vector? :hand vector?)
-  :ret int?)
+  :args (s/cat :board ::board :hand ::hand)
+  :ret ::game)
 
 (deftest test-dummy
   (is (= 0 0)))
